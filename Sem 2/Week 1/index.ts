90fa7da2857ce1ee938a2dbfb95f1fcc56e46bc5
justinlopezz <48078076+justinlopezz@users.noreply.github.com>
2019-07-29 13:20:44 +1000

    week 1

---
+++ Sem 2/Week 1/index.ts
@@ -0,0 +1,54 @@
+class TodoItem {
+    title: string;
+    constructor(title:string){
+        this.title = title;
+    }
+}
+
+let arr1: TodoItem[] = [];
+
+var view = 1
+
+while(view <=1){
+    var MyTodo1 = new TodoItem("sleep");
+    arr1.push(MyTodo1)
+    var MyTodo2 = new TodoItem("eat");
+    arr1.push(MyTodo2)
+    var MyTodo3 = new TodoItem("school");
+    arr1.push(MyTodo3)
+    var MyTodo4 = new TodoItem("Home");
+    arr1.push(MyTodo4)
+
+    console.log(arr1)
+
+    view++;
+}
+
+
+
+
+
+
+// let arr1: TodoItem[] = [];
+
+// var arr1 = ["Eat", ""]
+ 
+// arr1.push("Eat");
+
+// console.log(arr1);
+
+// var MyTodo = new TodoItem("Sleep");
+
+// var MyTodo = new TodoItem("Eat");
+
+// var MyTodo = new TodoItem("School");
+
+// var MyTodo = new TodoItem("Home");
+
+
+// var count = 0;
+
+// while (count <= 10) {
+//     console.log(count);
+//     count++;
+// }
