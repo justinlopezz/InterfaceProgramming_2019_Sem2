f9be597e59712239820c554b88d93b821a6582db
justinlopezz <48078076+justinlopezz@users.noreply.github.com>
2019-09-02 10:24:28 +1000

    lachie's todo (uses classes)

--- Sem 2/Week 6/Todo/src/app/app.component.ts
+++ Sem 2/Week 6/Todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  todos: Todo[] = [];
+
+  constructor() {
+    this.todos.push(new Todo("Completed this task", false));
+    this.todos.push(new Todo("Adding supported components", false));
+  }
+
+  addTodo(todo: Todo){
+    this.todos.push(todo);
+  }
+
   title = 'Todo';
 }
