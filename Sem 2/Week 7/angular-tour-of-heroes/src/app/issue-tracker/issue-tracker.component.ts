fecf003524b27caa15d422e829531517b17cf892
justinlopezz <48078076+justinlopezz@users.noreply.github.com>
2019-09-09 11:51:16 +1000

    Started to create an issue tracker

---
+++ Sem 2/Week 7/angular-tour-of-heroes/src/app/issue-tracker/issue-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-tracker',
+  templateUrl: './issue-tracker.component.html',
+  styleUrls: ['./issue-tracker.component.css']
+})
+export class IssueTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
